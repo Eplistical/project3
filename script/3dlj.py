b9c7997f2e9fcbc28df9fac8abcd303df53e8293
gaohan <gaohanm@gmail.com>
2018-07-17 12:17:26 -0400

    3dlj.py too slow, transfer to cpp

--- script/3dlj.py
+++ script/3dlj.py
@@ -13,16 +13,15 @@ from matplotlib import pyplot as plt
 # config
 kT = 1.0
 beta = 1.0 / kT
-mu = -2.5
+mu = -3.2
 V = 512
 L = V**(1.0 / 3.0)
 Vc = V
 Lc = Vc**(1.0 / 3.0)
 dxmax = 1.0
 
-Nrun = 1
 Nstep_eql = int(1e5)
-Nstep_run = int(3e5)
+Nstep_run = int(7e5)
 
 
 class LJ_with_cutoff(object):
@@ -33,26 +32,34 @@ class LJ_with_cutoff(object):
     def __init__(self):
         self.U_rc = self.LJ_raw(self.rc)
 
-    def __call__(self, r):
-        return 0.0
+    def __call__(self, r1, r2):
+        dr = r1 - r2
+        # minimum image 
+        for i, dri in enumerate(dr):
+            if dri > 0.5 * L:
+                dr[i] = L - dr[i]
+            elif dri < -0.5 * L:
+                dr[i] = L + dr[i]
+            if np.abs(dr[i]) > self.rc:
+                return 0.0
+        r = la.norm(dr)
         if r <= self.rc:
             return self.LJ_raw(r) - self.U_rc
         else:
             return 0.0
 
-    #@jit
+    @jit
     def LJ_raw(self, r):
         assert r > 0.0
-        tmp = 1.0 / r**6
+        tmp = r**(-6)
         return 4 * tmp * (tmp - 1)
 
 LJ = LJ_with_cutoff()
 
 
 def parse_args():
-    global Nstep_eql, Nstep_run, Nrun
+    global Nstep_eql, Nstep_run
     parser = argparse.ArgumentParser()
-    parser.add_argument('--Nrun', type=int, help="run times")
     parser.add_argument('--Nstep_eql', type=int, help="Nstep for equilibrium")
     parser.add_argument('--Nstep_run', type=int, help="Nstep for running")
     args = parser.parse_args()
@@ -60,37 +67,35 @@ def parse_args():
         Nstep_eql = args.Nstep_eql
     if args.Nstep_run is not None:
         Nstep_run = args.Nstep_run
-    if args.Nrun is not None:
-        Nrun = args.Nrun
 
 
 parse_args()
 print('# V = %.2f, Vc = %.2f' %(V, Vc))
-print('# Nstep_eql = %d, Nstep_run = %d, Nrun = %d' % (Nstep_eql, Nstep_run, Nrun))
+print('# Nstep_eql = %d, Nstep_run = %d' % (Nstep_eql, Nstep_run))
 
 
 # functions
-#@jit
+@jit
 def cal_U(swarm):
     N = cal_N(swarm)
     rst = 0.0
     for i in range(N - 1):
         for j in range(i + 1, N):
-            rst += LJ(la.norm(swarm[i] - swarm[j]))
+            rst += LJ(swarm[i], swarm[j])
     return rst
 
 
-#@jit
+@jit
 def cal_N(swarm):
     return len(swarm)
 
 
-#@jit
+@jit
 def is_in_Vc(ptcl):
     return np.all(np.logical_and(ptcl < Lc, ptcl > 0.0))
 
 
-#@jit
+@jit
 def cal_Nc(swarm):
     rst = 0
     for ptcl in swarm:
@@ -99,15 +104,15 @@ def cal_Nc(swarm):
     return rst
 
 
-#@jit
+@jit
 def rand_in_Vc():
     """generate a rancom position in Lc"""
     return np.random.rand(3) * Lc
 
 
-#@jit
+@jit
 def MC_step(swarm):
-    #swarm = shuffle(swarm)
+    swarm = shuffle(swarm)
     if np.random.rand() < 0.5:
         swarm = create(swarm)
     else:
@@ -115,12 +120,13 @@ def MC_step(swarm):
     return swarm
 
 
-#@jit
+@jit
 def shuffle(swarm):
-    new_swarm = list()
-    for k, ptcl in enumerate(swarm):
+    N = cal_N(swarm)
+    for k in range(N):
         dx = (np.random.rand(3) * 2.0 - 1.0) * dxmax
-        new_ptcl = np.copy(ptcl) + dx
+        new_ptcl = np.copy(swarm[k]) + dx
+
         # periodic condition
         for i in range(3):
             if new_ptcl[i] < 0.0:
@@ -128,27 +134,27 @@ def shuffle(swarm):
             elif new_ptcl[i] > L:
                 new_ptcl[i] -= L
         assert np.all(np.logical_and(new_ptcl >= 0.0, new_ptcl <= L))
+
         # calc dU
         dU = 0.0
-        for j, ptcl_j in enumerate(swarm):
+        for j in range(N):
             if j != k:
-                dU += LJ(la.norm(new_ptcl - ptcl_j)) - LJ(la.norm(ptcl - ptcl_j))
+                dU += LJ(new_ptcl, swarm[j]) - LJ(swarm[k], swarm[j])
+
         # make decision
         prob = np.exp(-beta * dU)
         if np.random.rand() < prob:
-            new_swarm.append(new_ptcl)
-        else:
-            new_swarm.append(ptcl)
-    return new_swarm
+            swarm[k] = new_ptcl
+    return swarm
 
 
-#@jit
+@jit
 def create(swarm):
     new_ptcl = rand_in_Vc()
     # cal dU
     dU = 0.0
     for i, ptcl in enumerate(swarm):
-        dU += LJ(la.norm(new_ptcl - ptcl))
+        dU += LJ(new_ptcl, ptcl)
     # make decision
     Nc = cal_Nc(swarm)
     prob = np.exp(-beta * dU + beta * mu) * Vc / (Nc + 1)
@@ -157,7 +163,7 @@ def create(swarm):
     return swarm
 
 
-#@jit
+@jit
 def destruct(swarm):
     Nc = cal_Nc(swarm)
     if Nc > 0:
@@ -172,7 +178,7 @@ def destruct(swarm):
         dU = 0.0
         for i, ptcl in enumerate(swarm):
             if i != idx:
-                dU -= LJ(la.norm(ptcl - swarm[idx]))
+                dU -= LJ(ptcl, swarm[idx])
         # make decision
         prob = np.exp(-beta * dU - beta * mu) * Nc / Vc
         if np.random.rand() < prob:
@@ -180,22 +186,22 @@ def destruct(swarm):
     return swarm
 
 
-#@jit
+@jit
 def main():
-    for irun in range(Nrun):
-        swarm = list()
-        swarm.append(np.array([L/2, L/2, L/2]))
+    swarm = list()
+    swarm.append(np.array([L/2, L/2, L/2]))
 
-        # equilibrate
-        for istep in range(Nstep_eql):
-            swarm = MC_step(swarm)
+    # equilibrate
+    for istep in range(Nstep_eql):
+        print(istep)
+        swarm = MC_step(swarm)
 
-        # collect data
-        rho = 0.0
-        for istep in range(Nstep_run):
-            rho += cal_N(swarm) / V
-            print(istep, rho / (istep + 1))
-            swarm = MC_step(swarm)
+    # collect data
+    rho = 0.0
+    for istep in range(Nstep_run):
+        rho += cal_N(swarm) / V
+        print(istep, rho / (istep + 1))
+        swarm = MC_step(swarm)
 
     # output
 
